046f7459741dedf3feea78e2a6f8a6f25519e985
iankurao <kuraoian@gmail.com>
2019-06-03 10:08:01 +0300

    initialize our flask migrate extensinon to allow migrations

--- manage.py
+++ manage.py
@@ -10,6 +10,9 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 
 @manager.shell
 def make_shell_context():
