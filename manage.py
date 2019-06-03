3210b6099f213a7dfa24cdaade7205e95a09474b
iankurao <kuraoian@gmail.com>
2019-06-03 10:00:58 +0300

    create a test case to check password hashing

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-
+from app.models import User,Role,Comment,Blog
 
 #Create app instance
 app = create_app('test')
@@ -15,6 +15,11 @@ manager.add_command('server',Server)
 def make_shell_context():
     return dict(app=app, db=db, User = User,Role = Role, Comment = Comment, Blog = Blog)
 
+@manager.command
+def test():
+    import unittest
+    tests=unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
