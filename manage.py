663c5e74c6bf3973837b9fefa6329479b16a6a4c
iankurao <kuraoian@gmail.com>
2019-06-03 15:36:35 +0300

    change application structure

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role,Comment,Blog
+from app.models import User,Role,Comments,Blogs
+from flask_migrate import Migrate,MigrateCommand
 
 #Create app instance
 app = create_app('test')
-app=create_app("production")
+# app=create_app("production")
 app = create_app('development')
 
 manager = Manager(app)
@@ -16,7 +17,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User = User,Role = Role, Comment = Comment, Blog = Blog)
+    return dict(app=app, db=db, User = User,Role = Role, Comments = Comments, Blogs = Blogs)
 
 @manager.command
 def test():
