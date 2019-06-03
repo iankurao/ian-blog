083245409dc5f5f25cdb8630f4f11fee02d35682
iankurao <kuraoian@gmail.com>
2019-06-03 10:14:18 +0300

    secure passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -14,16 +15,16 @@ class User(UserMixin,db.Model):
     blogs = db.relationship('Blog',backref = 'user', lazy = 'dynamic')
     comments = db.relationship('Comment',backref = 'user', lazy = 'dynamic')
 
-    # @property
-    # def password(self):
-    #     raise AttributeError('You cannot read the password attritube')
+     @property
+     def password(self):
+         raise AttributeError('You cannot read the password attritube')
 
-    # @password.setter
-    # def password(self, password):
-    #     self.password_hash = generate_password_hash(password)
+     @password.setter
+     def password(self, password):
+         self.password_hash = generate_password_hash(password)
 
-    # def verify_password(self,password):
-    #     return check_password_hash(self.password_hash,password)
+     def verify_password(self,password):
+         return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
