766c71d6028a22dd363fe11678abd82da3c426ea
iankurao <kuraoian@gmail.com>
2019-06-03 09:13:51 +0300

    register main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
@@ -7,9 +10,17 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
 
-
+    # App Configurations
+    app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+
+
+    # Register main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app 
 
