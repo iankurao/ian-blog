978644c50cb4801a5e1671ff62c8c5dc0e91a337
iankurao <kuraoian@gmail.com>
2019-06-03 08:47:01 +0300

    configure application with database

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY='123'
+
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ian:vionashina@localhost/pitch_test'
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ian:vionashina@localhost/pitch'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+
